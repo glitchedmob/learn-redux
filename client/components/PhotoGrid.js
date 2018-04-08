da366143395bd72c93b4e708a53a026b6aed3ab8
Levi Zitting <levizitting5663@gmail.com>
2018-04-08 10:24:01 -0500

    Component and router setup

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const PhotoGrid = React.createClass({
+	render() {
+		return (
+			<div className="photo-grid">
+
+			</div>
+		)
+	}
+});
---
+++ client/components/Single.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Single = React.createClass({
+	render() {
+		return (
+			<div className="single-photo">
+
+			</div>
+		)
+	}
+});
